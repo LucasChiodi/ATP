9749f730dac1fce0264efc83a5f58af39499cce6
Lucas <lucaschiodi11@gmail.com>
2024-06-29 18:03:42 -0300

    teste

--- lista6/obj/Debug/net8.0/atp.AssemblyInfo.cs
+++ lista6/obj/Debug/net8.0/atp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("atp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+478a51cf6f6c0b93da60db7efc7861b0570c982d")]
 [assembly: System.Reflection.AssemblyProductAttribute("atp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("atp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
